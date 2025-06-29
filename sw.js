c89d360cb61558cb9cc5b130a5c914ee3ed8fe55
Havi00 <havinassh0402@student.usm.my>
2025-06-29 22:55:52 +0800

    Add background love song (autoplay + offline)

--- sw.js
+++ sw.js
@@ -8,10 +8,11 @@ self.addEventListener("install", e => {
         "/love-site/js/typer.js",
         "/love-site/songs.html",
         "/love-site/message.html",
-        "/love-site/padlet.html",
+        "/love-site/photos.html",
         "/love-site/images/background.jpg",
         "/love-site/images/icon-192.png",
-        "/love-site/images/icon-512.png"
+        "/love-site/images/icon-512.png",
+        "/love-site/audio/love.mp3"         // 💾 cache music for offline
       ]);
     })
   );
@@ -19,6 +20,6 @@ self.addEventListener("install", e => {
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
